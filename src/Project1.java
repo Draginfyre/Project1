434e78ac6828b0a958192b4cbc1a2a1fbc36f643
Gabrielle <gbarrantes11@gmail.com>
2022-01-16 18:54:59 -0500

    project upload

--- src/Project1.java
+++ src/Project1.java
@@ -9,7 +9,7 @@ public class Project1 {
     public static void main(String[] args)
     {
         System.out.println(
-                "You are in a land full of dragons. In front of you, you see two caves.\n" +
+                "You are in a land full of dragons. In front of you, you see two caves./n" +
                         "In one cave, the dragon is friendly and will share his treasure with you./n" +
                         "The other dragon is greedy and hungry and will eat you on sight./n" +
                         "Which cave will you go into? (1 or 2)"
@@ -22,9 +22,9 @@ public class Project1 {
         {
             if (input.equals("1"))
             {
-                System.out.println("You approach the cave...\n"+
-                        "It is dark and spooky...\n"+
-                        "A large dragon jumps out in front of you! He opens his jaws and...\n"+
+                System.out.println("You approach the cave.../n"+
+                        "It is dark and spooky.../n"+
+                        "A large dragon jumps out in front of you! He opens his jaws and.../n"+
                         "Gobbles you down in one bite!");
             }else if (input.equals("2"))
             {
