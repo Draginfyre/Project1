3a440e585ddf1d8872abe447e01b43b814b76e44
Draginfyre <gbarrantes11@gmail.com>
2022-01-16 19:26:27 -0500

    Update Project1.java

--- src/Project1.java
+++ src/Project1.java
@@ -9,7 +9,7 @@ public class Project1 {
     public static void main(String[] args)
     {
         System.out.println(
-                "You are in a land full of dragons. In front of you, you see two caves.\n" +
+                "You are in a land full of dragons. In front of you, you see two caves./n" +
                         "In one cave, the dragon is friendly and will share his treasure with you./n" +
                         "The other dragon is greedy and hungry and will eat you on sight./n" +
                         "Which cave will you go into? (1 or 2)"
